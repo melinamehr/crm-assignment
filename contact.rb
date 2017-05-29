f5e37b454600128dd71891f565c7cb09c379bb98
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-05-29 13:13:18 -0400

    initiallizing database

--- contact.rb
+++ contact.rb
@@ -1,3 +1,13 @@
+gem 'activerecord', '=4.2.7'
+require 'active_record'
+require 'mini_record'
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm.sqlite3')
+
+class Contact
+...
+end
+
 class Contact
   attr_accessor :first_name, :last_name, :email, :note
   attr_reader :id
